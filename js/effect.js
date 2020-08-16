2d73afa335300b1672eb3ce1382c04630e4d02b2
Kaique <kaiqueq147@gmail.com>
2020-08-16 13:41:45 -0300

    addEffects

--- js/effect.js
+++ js/effect.js
@@ -9,3 +9,23 @@ function typeWriter(element) {
 const spec = document.querySelector('.section-intro .info span')
 
 typeWriter(spec)
+initScrollEffect()
+
+function initScrollEffect() {
+    const scrollSection = document.querySelectorAll('.js-scroll')
+
+    function scrollEffect() {
+
+        scrollSection.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top
+            const windowHalf = window.innerHeight * 0.6
+            const isVisible = (sectionTop - windowHalf) < 0
+            if (isVisible)
+                section.classList.add('ativo')
+            else
+                section.classList.remove('ativo')
+        })
+    }
+    scrollEffect()
+    window.addEventListener('scroll', scrollEffect)
+}
